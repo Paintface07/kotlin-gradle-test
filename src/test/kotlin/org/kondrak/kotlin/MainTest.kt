07698db6ffef93959761e81138c7b2b20431fa3b
Kevin Kondratuk <paintface07@gmail.com>
2016-02-23 21:16:36 -0600

    add dummy tests

---
+++ src/test/kotlin/org/kondrak/kotlin/MainTest.kt
@@ -0,0 +1,20 @@
+package org.kondrak.kotlin
+
+import org.junit.Test
+import org.kondrak.kotlin.main
+import kotlin.test.*
+
+/**
+ * Created by Administrator on 2/23/2016.
+ */
+ public class JUnit4MainTest {
+    @Test fun testTrue() {
+        // Do I know what's real?
+        assertEquals(true, true)
+    }
+
+    @Test fun testGetter() {
+        // Can I test things?
+        assertEquals("Hello World!", stringGetter())
+    }
+}
